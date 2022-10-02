ba6a4e319412c5a9dbc283df511e3f274ce8fce4
jciberlin <jciberlin1@gmail.com>
2022-10-02 19:51:48 +0200

    Implement queue that is MISRA compliant
    - MISRA compliant queue

---
+++ Src/queue.c
@@ -0,0 +1,106 @@
+/****************************************************************************
+ *
+ *   Copyright (c) 2022 IMProject Development Team. All rights reserved.
+ *   Authors: Juraj Ciberlin <jciberlin1@gmail.com>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name IMProject nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+#include "queue.h"
+
+#include <string.h>
+#include <stdint.h>
+
+void
+Queue_initQueue(Queue_t* const queue, const int capacity, const unsigned int element_size, void* buffer) {
+    queue->capacity = capacity;
+    queue->front = 0U;
+    queue->size = 0U;
+    queue->rear = capacity - 1;
+    queue->element_size = element_size;
+    queue->buffer = &buffer;
+}
+
+bool
+Queue_isFull(const Queue_t* const queue) {
+    return (queue->size == queue->capacity);
+}
+
+bool
+Queue_isEmpty(const Queue_t* const queue) {
+    return (queue->size == 0U);
+}
+
+bool
+Queue_enqueue(Queue_t* const queue, const void* const element) {
+    bool status = false;
+    if (!Queue_isFull(queue)) {
+        status = true;
+        queue->rear = (queue->rear + 1) % (int)queue->capacity;
+        uint8_t* buffer = (uint8_t*)(&queue->buffer);
+        (void*)memcpy(&buffer[queue->rear * (int)queue->element_size], element, queue->element_size);
+        queue->size = queue->size + 1U;
+    }
+    return status;
+}
+
+bool
+Queue_dequeue(Queue_t* const queue, void* const element) {
+    bool status = false;
+    if (!Queue_isEmpty(queue)) {
+        status = true;
+        uint8_t* buffer = (uint8_t*)(&queue->buffer);
+        (void*)memcpy(element, &buffer[queue->front * queue->element_size], queue->element_size);
+        queue->front = (queue->front + 1U) % queue->capacity;
+        queue->size = queue->size - 1U;
+    }
+    return status;
+}
+
+bool
+Queue_front(Queue_t* const queue, void* const element) {
+    bool status = false;
+    if (!Queue_isEmpty(queue)) {
+        status = true;
+        uint8_t* buffer = (uint8_t*)(&queue->buffer);
+        (void*)memcpy(element, &buffer[queue->front * queue->element_size], queue->element_size);
+    }
+    return status;
+}
+
+bool
+Queue_rear(Queue_t* const queue, void* const element) {
+    bool status = false;
+    if (!Queue_isEmpty(queue)) {
+        status = true;
+        uint8_t* buffer = (uint8_t*)(&queue->buffer);
+        (void*)memcpy(element, &buffer[queue->rear * (int)queue->element_size], queue->element_size);
+    }
+    return status;
+}
---
+++ Tests/test_queue.c
@@ -0,0 +1,97 @@
+#include "queue.h"
+#include "unity.h"
+#include "unity_fixture.h"
+
+TEST_GROUP(Queue);
+
+TEST_SETUP(Queue) {
+}
+
+TEST_TEAR_DOWN(Queue) {
+}
+
+TEST_GROUP_RUNNER(Queue) {
+    RUN_TEST_CASE(Queue, Queue_enqueue_dequeue_uint32);
+    RUN_TEST_CASE(Queue, Queue_enqueue_dequeue_float);
+}
+
+TEST(Queue, Queue_enqueue_dequeue_uint32) {
+    uint32_t array[100];
+    const unsigned int capacity = sizeof(array) / sizeof(array[0]);
+    Queue_t queue;
+    Queue_initQueue(&queue, capacity, sizeof(array[0]), array);
+
+    TEST_ASSERT_FALSE(Queue_isFull(&queue));
+    TEST_ASSERT_TRUE(Queue_isEmpty(&queue));
+    uint32_t element;
+    TEST_ASSERT_FALSE(Queue_dequeue(&queue, &element));
+
+    // fill the queue
+    uint32_t i;
+    for (i = 0U; i < capacity; ++i) {
+        TEST_ASSERT_TRUE(Queue_enqueue(&queue, &i));
+    }
+
+    // queue is full
+    TEST_ASSERT_TRUE(Queue_isFull(&queue));
+    TEST_ASSERT_FALSE(Queue_enqueue(&queue, &i));
+
+    // check front element
+    TEST_ASSERT_TRUE(Queue_front(&queue, &element));
+    TEST_ASSERT_EQUAL_UINT32(0U, element);
+
+    // check rear element
+    TEST_ASSERT_TRUE(Queue_rear(&queue, &element));
+    TEST_ASSERT_EQUAL_UINT32(i - 1U, element);
+
+    // dequeue
+    TEST_ASSERT_TRUE(Queue_dequeue(&queue, &element));
+    TEST_ASSERT_EQUAL_UINT32(0U, element);
+
+    // enqueue
+    TEST_ASSERT_TRUE(Queue_enqueue(&queue, &i));
+    TEST_ASSERT_TRUE(Queue_rear(&queue, &element));
+    TEST_ASSERT_EQUAL_UINT32(i, element);
+}
+
+TEST(Queue, Queue_enqueue_dequeue_float) {
+    float array[100];
+    const unsigned int capacity = sizeof(array) / sizeof(array[0]);
+    Queue_t queue;
+    Queue_initQueue(&queue, capacity, sizeof(array[0]), array);
+
+    TEST_ASSERT_FALSE(Queue_isFull(&queue));
+    TEST_ASSERT_TRUE(Queue_isEmpty(&queue));
+    float element = 1.0f;
+    TEST_ASSERT_FALSE(Queue_dequeue(&queue, &element));
+
+    // fill the queue
+    uint32_t i;
+    for (i = 0U; i < capacity; ++i) {
+        float element_temp = (float)i + 1.1f;
+        TEST_ASSERT_TRUE(Queue_enqueue(&queue, &element_temp));
+    }
+
+    // queue is full
+    TEST_ASSERT_TRUE(Queue_isFull(&queue));
+    TEST_ASSERT_FALSE(Queue_enqueue(&queue, &element));
+
+    // check front element
+    TEST_ASSERT_TRUE(Queue_front(&queue, &element));
+    TEST_ASSERT_EQUAL_FLOAT(1.1f, element);
+
+    // check rear element
+    TEST_ASSERT_TRUE(Queue_rear(&queue, &element));
+    TEST_ASSERT_EQUAL_FLOAT((float)capacity - 1.0f + 1.1f, element);
+
+    // dequeue
+    TEST_ASSERT_TRUE(Queue_dequeue(&queue, &element));
+    TEST_ASSERT_EQUAL_FLOAT(1.1f, element);
+
+    // enqueue
+    element = 5.1f;
+    TEST_ASSERT_TRUE(Queue_enqueue(&queue, &element));
+    float test_element;
+    TEST_ASSERT_TRUE(Queue_rear(&queue, &test_element));
+    TEST_ASSERT_EQUAL_UINT32(test_element, element);
+}
