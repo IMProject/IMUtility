797817515ae914ab0d8e8654da407f7eb753a6d9
jciberlin <jciberlin1@gmail.com>
2023-04-01 18:34:59 +0200

    base64: Add unit tests
    - add unit tests to cover all cases

--- Tests/test_base64.c
+++ Tests/test_base64.c
@@ -18,6 +18,7 @@ TEST_GROUP_RUNNER(Base64) {
 
 TEST(Base64, Base64_encode) {
     {
+        // Success
         const char string[] = "IMProject is a very cool project!";
         char base64[200];
         int32_t success = Base64_encode(string, strlen(string), base64, sizeof(base64));
@@ -32,10 +33,19 @@ TEST(Base64, Base64_encode) {
         TEST_ASSERT_EQUAL_INT32(1, success);
     }
     {
+        // Success
         const char string[] = "Hehe";
         char base64[200];
         int32_t success = Base64_encode(string, strlen(string), base64, sizeof(base64));
         TEST_ASSERT_EQUAL_STRING("SGVoZQ==", base64);
+        TEST_ASSERT_EQUAL_INT32(0, success);
+    }
+    {
+        // Success
+        const char string[] = "jc";
+        char base64[200];
+        int32_t success = Base64_encode(string, strlen(string), base64, sizeof(base64));
+        TEST_ASSERT_EQUAL_STRING("amM=", base64);
         TEST_ASSERT_EQUAL_INT32(0, success);
     }
     {
@@ -45,10 +55,18 @@ TEST(Base64, Base64_encode) {
         int32_t success = Base64_encode(string, strlen(string), base64, sizeof(base64));
         TEST_ASSERT_EQUAL_INT32(1, success);
     }
+    {
+        // data_length = 18446744073709551610, overflow
+        const char string[] = "Hehe";
+        char base64[4];
+        int32_t success = Base64_encode(string, 18446744073709551610U, base64, sizeof(base64));
+        TEST_ASSERT_EQUAL_INT32(1, success);
+    }
 }
 
 TEST(Base64, Base64_decode) {
     {
+        // Success
         char text[] = "IMProject is a very cool project!";
         char base64[] = "SU1Qcm9qZWN0IGlzIGEgdmVyeSBjb29sIHByb2plY3Qh";
         uint8_t string[200];
@@ -64,8 +82,27 @@ TEST(Base64, Base64_decode) {
         TEST_ASSERT_EQUAL_INT32(1, success);
     }
     {
+        // Success
         char text[] = "B?E(H+MbQeThWmZq4t7w!z%C*F)J@NcR";
         char base64[] = "Qj9FKEgrTWJRZVRoV21acTR0N3cheiVDKkYpSkBOY1I=";
+        uint8_t string[200];
+        int32_t success = Base64_decode(base64, strlen(base64), string, sizeof(string));
+        TEST_ASSERT_EQUAL_MEMORY(text, string, strlen(text));
+        TEST_ASSERT_EQUAL_INT32(0, success);
+    }
+    {
+        // Success
+        char text[] = "Hehe";
+        char base64[] = "SGVoZQ==";
+        uint8_t string[200];
+        int32_t success = Base64_decode(base64, strlen(base64), string, sizeof(string));
+        TEST_ASSERT_EQUAL_MEMORY(text, string, strlen(text));
+        TEST_ASSERT_EQUAL_INT32(0, success);
+    }
+    {
+        // Success
+        char text[] = "Hehe";
+        char base64[] = "SGVoZQ1==";
         uint8_t string[200];
         int32_t success = Base64_decode(base64, strlen(base64), string, sizeof(string));
         TEST_ASSERT_EQUAL_MEMORY(text, string, strlen(text));
@@ -76,6 +113,13 @@ TEST(Base64, Base64_decode) {
         char base64[] = "Qj9FKEgrTWJRZVRoV21acTR0N3cheiVDKkYpSkBOY1I=";
         uint8_t string[31];
         int32_t success = Base64_decode(base64, strlen(base64), string, sizeof(string));
+        TEST_ASSERT_EQUAL_INT32(1, success);
+    }
+    {
+        // Fail, length = 0
+        char base64[] = "SGVoZQ==";
+        uint8_t string[200];
+        int32_t success = Base64_decode(base64, 0U, string, sizeof(string));
         TEST_ASSERT_EQUAL_INT32(1, success);
     }
 }
